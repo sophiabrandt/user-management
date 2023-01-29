8fc20f8988c445ae40a402e219df5140bddfbfcb
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-01-29 15:34:30 +0100

    feat: add form styling and required

--- src/app/users/users-edit/ui/users-edit-form.component.ts
+++ src/app/users/users-edit/ui/users-edit-form.component.ts
@@ -5,7 +5,6 @@ import {
   Input,
   OnInit,
   Output,
-  ViewChild,
 } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { PocketBaseUser, User } from '../../../shared/interfaces/user';
@@ -15,26 +14,125 @@ import { PocketBaseUser, User } from '../../../shared/interfaces/user';
   standalone: true,
   imports: [FormsModule],
   template: `
-    <form
-      #form="ngForm"
-      (submit)="submit()"
-      class="form-control w-full max-w-xs"
-    >
-      <label class="label">
-        <span class="label-text">Name</span>
-      </label>
-      <input
-        name="formName"
-        [(ngModel)]="usersEditFormModel.name"
-        type="text"
-        placeholder="My name"
-        class="input input-bordered w-full max-w-xs"
-      />
-      <label class="label"> </label>
-      <button type="submit" class="btn btn-wide">Submit</button>
-    </form>
+    <div class="m-2">
+      <form
+        #form="ngForm"
+        (submit)="onSubmit(form)"
+        class="users-edit-form center form form-control w-full max-w-xs"
+      >
+        <div class="form-group">
+          <label class="label">
+            <span class="label-text">Name</span>
+          </label>
+          <input
+            required
+            minlength="2"
+            pattern="^[a-zA-z- ]+$"
+            name="name"
+            [(ngModel)]="usersEditFormModel.name"
+            type="text"
+            placeholder="My name"
+            class="input input-bordered"
+          />
+        </div>
+        <div class="form-group">
+          <label class="label">
+            <span class="label-text">Surname</span>
+          </label>
+          <input
+            required
+            minlength="2"
+            pattern="^[a-zA-z- ]+$"
+            name="surname"
+            [(ngModel)]="usersEditFormModel.surname"
+            type="text"
+            placeholder="My last name"
+            class="input input-bordered"
+          />
+        </div>
+        <div class="form-group form-group--full-width">
+          <label class="label">
+            <span class="label-text">Email</span>
+          </label>
+          <input
+            required
+            email
+            name="email"
+            [(ngModel)]="usersEditFormModel.email"
+            type="email"
+            placeholder="email@example.com"
+            class="input input-bordered"
+          />
+        </div>
+        <div class="form-group">
+          <label class="label">
+            <span class="label-text">Position</span>
+          </label>
+          <input
+            required
+            minlength="2"
+            pattern="^[a-zA-z- ]+$"
+            name="position"
+            [(ngModel)]="usersEditFormModel.position"
+            type="text"
+            placeholder="CEO"
+            class="input input-bordered"
+          />
+        </div>
+        <div class="form-group">
+          <label class="label">
+            <span class="label-text">Location</span>
+          </label>
+          <input
+            required
+            minlength="2"
+            pattern="^[a-zA-z- ]+$"
+            name="location"
+            [(ngModel)]="usersEditFormModel.location"
+            type="text"
+            placeholder="remote"
+            class="input input-bordered"
+          />
+        </div>
+        <button
+          [disabled]="form.invalid"
+          type="submit"
+          class="form-group form-group--full-width w-full btn btn-wide disabled:btn-disabled"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   `,
-  styles: [],
+  styles: [
+    `
+      .users-edit-form {
+        display: grid;
+        gap: 1.25rem;
+      }
+
+      @media (min-width: 35em) {
+        .users-edit-form {
+          grid-template-columns: 1fr 1fr;
+        }
+      }
+
+      .form-group {
+        display: grid;
+        gap: 0.5rem;
+      }
+
+      .form-group--full-width {
+        grid-column: 1/-1;
+      }
+      .ng-valid.ng-dirty:not([ngModelGroup]):not(form) {
+        border: 2px solid yellowgreen;
+      }
+      .ng-invalid.ng-dirty:not([ngModelGroup]):not(form) {
+        border: 2px solid tomato;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UsersEditFormComponent implements OnInit {
@@ -42,19 +140,18 @@ export class UsersEditFormComponent implements OnInit {
 
   @Output() editUser = new EventEmitter<PocketBaseUser>();
 
-  @ViewChild(NgForm) form: NgForm;
-
   usersEditFormModel: User;
 
   ngOnInit(): void {
     this.usersEditFormModel = this.user;
   }
 
-  submit(): void {
-    if (this.form.valid) {
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      console.log(form.value);
       this.editUser.emit(this.user);
     } else {
-      this.form.form.markAllAsTouched();
+      form.form.markAllAsTouched();
     }
   }
 }
